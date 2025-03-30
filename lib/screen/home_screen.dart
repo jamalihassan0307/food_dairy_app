db3ff7a07a3f49e401b78807155fcef1c4f73632
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-30 08:59:47 +0500

    Refactor HomeScreen to use GNav for bottom navigation, replacing ButtonNavigationBar. Added page change handling and updated state management for selected index.

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 
-import 'package:button_navigation_bar/button_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:food_dairy_app/controller/recipe_repository.dart';
 import 'package:food_dairy_app/controller/sqllite.dart';
@@ -10,6 +9,7 @@ import 'package:food_dairy_app/screen/addfood.dart';
 import 'package:food_dairy_app/screen/nev_bar/profile.dart';
 import 'package:food_dairy_app/screen/settingScreen.dart';
 import 'package:get/get.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -27,6 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   PageController page = PageController();
+  int _selectedIndex = 0;
 
   var height, width;
   @override
@@ -37,32 +38,60 @@ class _HomeScreenState extends State<HomeScreen> {
       resizeToAvoidBottomInset: true,
       body: PageView(
         controller: page,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
         children: const [Page1(), Page2(), SettingsScreen()],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: ButtonNavigationBar(
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-        children: [
-          ButtonNavigationItem(
-            icon: const Icon(Icons.map_outlined),
-            onPressed: () {
-              page.jumpToPage(0);
-            },
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: Colors.black,
+              iconSize: 24,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: const Duration(milliseconds: 400),
+              tabBackgroundColor: Colors.grey[100]!,
+              color: Colors.black,
+              tabs: const [
+                GButton(
+                  icon: Icons.map_outlined,
+                  text: 'Map',
+                ),
+                GButton(
+                  icon: Icons.data_exploration_outlined,
+                  text: 'Data',
+                ),
+                GButton(
+                  icon: Icons.person,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                  page.jumpToPage(index);
+                });
+              },
+            ),
           ),
-          ButtonNavigationItem(
-            width: width * 0.3,
-            icon: const Icon(Icons.data_exploration_outlined),
-            onPressed: () {
-              page.jumpToPage(1);
-            },
-          ),
-          ButtonNavigationItem(
-            icon: const Icon(Icons.person),
-            onPressed: () {
-              page.jumpToPage(2);
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
